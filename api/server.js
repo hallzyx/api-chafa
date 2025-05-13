33e99bc0ba8ab2ad48c61a09b7c42a7325c824a5
Brayan Roberto Ccarita Cruz <hallinxsl7@gmail.com>
2025-05-13 05:58:55 -0500

    cors re again

--- api/server.js
+++ api/server.js
@@ -1,19 +1,20 @@
 // See https://github.com/typicode/json-server#module
 const jsonServer = require('json-server')
 const server = jsonServer.create()
-const router = jsonServer.router('db.json')
+const router = jsonServer.router('../db.json')
 const middlewares = jsonServer.defaults()
 const cors = require('cors')
 server.use(middlewares)
 // Add this before server.use(router)
 server.use(jsonServer.rewriter({
-    '/api/v1/*': '/$1',
-    '/product/:resource/:id/show': '/:resource/:id'
+    '/api/v1/*': '/$1'
 }))
 
 server.use(cors({
     origin: ['http://localhost:5173', 'https://aaaavue-git-main-hallzyxs-projects.vercel.app']
   }))
+
+server.use(jsonServer.bodyParser)
 
 server.use(router)
 server.listen(3001, () => {
