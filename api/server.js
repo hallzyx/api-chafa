637d5e20c93635bc6b6c268c166767add85d0625
Brayan Roberto Ccarita Cruz <hallinxsl7@gmail.com>
2025-05-13 06:18:20 -0500

    cors ten piedas

--- api/server.js
+++ api/server.js
@@ -2,7 +2,7 @@
 const jsonServer = require('json-server')
 const cors = require('cors')
 const server = jsonServer.create()
-const router = jsonServer.router('../db.json')
+const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
 
 server.use(cors({
